ee8d50ca2b3dcf3ad6a8743c6e354b3b058fb5bb
Aras Mumcuyan <arasmumcuyan@sabanciuniv.edu>
2018-05-03 12:47:27 +0200

    convert data generation to torch imp

--- generate_data.py
+++ generate_data.py
@@ -3,34 +3,39 @@ import torch
 import matplotlib.pyplot as plt
 
 
-def generate_data(minn=0, maxx=1, radius=1 / np.sqrt(2 * np.pi), center=np.array([0.5, 0.5]), num_of_points=1000,
-                  is_torch=False):
-    points = np.random.uniform(low=minn, high=maxx, size=(num_of_points, 2))
-    labels = (np.sum(np.square(center - points), axis=1) <= np.square(radius)).astype(np.float)
+def get_labels(points: torch.FloatTensor, center: torch.FloatTensor, radius: float):
+    num_of_points = points.shape[0]
 
-    if is_torch:
-        points = torch.from_numpy(points)
-        labels = torch.from_numpy(labels)
-        return points.type(torch.FloatTensor), labels.type(torch.FloatTensor)
-
-    return points, labels
+    labels = torch.FloatTensor(num_of_points).fill_(1)
+    labels[torch.pow(points - center, 2).sum(1) <= radius ** 2] = 0
+    return labels
 
 
-def generate_grid_data(minn=0, maxx=1, num_of_points_per_dim=51, is_torch=False):
-    coor_x = np.linspace(minn, maxx, num_of_points_per_dim)
-    coor_y = np.linspace(minn, maxx, num_of_points_per_dim)
-    xx, yy = np.meshgrid(coor_x, coor_y)
-    points = np.array([xx.flatten(), yy.flatten()]).T
+def generate_data(minn=0, maxx=1, radius=1 / np.sqrt(2 * np.pi), center=(0.5, 0.5), num_of_points=1000):
 
-    center = np.array([minn + maxx/2, (minn + maxx)/2])
-    radius = 1
-    labels = (np.sum(np.square(center - points), axis=1) <= np.square(radius)).astype(np.float)
+    center = torch.FloatTensor([center[0], center[1]])
+    points = torch.Tensor(num_of_points, 2).uniform_(minn, maxx)
+    return points, get_labels(points, center, radius)
 
-    if is_torch:
-        points, labels = torch.from_numpy(points), torch.from_numpy(labels)
-        return points.type(torch.FloatTensor), labels.type(torch.FloatTensor)
 
-    return points, labels
+def generate_grid(x, y):
+    h = x.shape[0]
+    w = y.shape[0]
+    grid = torch.stack([x.repeat(w), y.repeat(h, 1).t().contiguous().view(-1)],1)
+    return grid
+
+
+def generate_grid_data(minn=0, maxx=1, num_of_points_per_dim=51, radius=1 / np.sqrt(2 * np.pi)):
+
+    coor_x = torch.linspace(minn, maxx, num_of_points_per_dim)
+    coor_y = torch.linspace(minn, maxx, num_of_points_per_dim)
+
+    points = generate_grid(coor_x, coor_y)
+
+    val = (minn + maxx) / 2
+    center = torch.FloatTensor([val, val])
+    print("ccenter: {}".format(center))
+    return points, get_labels(points, center, radius)
 
 
 def plot_data(points, labels=0, minn=0, maxx=1, radius=1 / np.sqrt(2 * np.pi), center=np.array([0.5, 0.5])):
@@ -41,8 +46,5 @@ def plot_data(points, labels=0, minn=0, maxx=1, radius=1 / np.sqrt(2 * np.pi), c
     ax.set_xlim(minn, maxx)
     ax.set_ylim(minn, maxx)
 
-
-def main():
-    generate_data(is_torch=True)
-
-
+# generate_data()
+generate_grid_data()
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from modules.losses import LossSoftmaxCrossEntropy
 from optimizers import SGD
 
 
-def default_net_1(x_train, y_train, num_of_neurons=(2, 25, 25, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
+def default_net_1(x_all, y_all, num_of_neurons=(2, 25, 25, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
     ce = LossSoftmaxCrossEntropy()
 
     model = Sequential()
@@ -19,17 +19,16 @@ def default_net_1(x_train, y_train, num_of_neurons=(2, 25, 25, 25, 2), lr=0.1, m
     model.loss = ce
     sgd = SGD(lr, momentum_coef, weight_decay=0.2)
 
-    sgd.train(model, x_train, y_train, num_of_epochs)
+    sgd.train(model, x_all, y_all, num_of_epochs, val_split=0.2)
 
     return model, ce.loss_logging
 
 
-x_train, y_train = generate_data(is_torch=True, num_of_points=500)
-y_train = one_hot_label(y_train, val=0)  # convert labels to 1-hot encoding
+x_all, y_all = generate_data(is_torch=True, num_of_points=500)
+y_all = one_hot_label(y_all, val=0)  # convert labels to 1-hot encoding
 
-print("X_train.type: {} y_train.shape: {}".format(x_train.shape, y_train.shape))
-print("y_train: {}".format(y_train))
-model, loss1 = default_net_1(x_train, y_train, num_of_epochs=1000)
+print("X_train.type: {} y_train.shape: {}".format(x_all.shape, y_all.shape))
+model, loss1 = default_net_1(x_all, y_all, num_of_epochs=1000)
 
 
 x_test, y_test = generate_grid_data(minn=0, maxx=1, num_of_points_per_dim=51, is_torch=True)
