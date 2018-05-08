ca6cc3f0a68352ad8261e89f8713a848cca059d2
afofa <34076831+afofa@users.noreply.github.com>
2018-05-08 13:10:06 +0200

    update networks and utils
    update notebooks
    add defaults nets in networks.py
    add plotting functions in utils.py

--- networks.py
+++ networks.py
@@ -4,119 +4,180 @@ from modules.activations import ReLU, Tanh
 from modules.losses import LossMSE, LossCrossEntropy
 from optimizers.sgd import SGD
 
+def default_net(x_all, y_all, num_of_hidden_layers=3, loss='ce', num_of_neurons=(2, 25, 25, 25, 2), activation='relu', lr=0.1, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    generic
+    """
+    if loss == 'ce':
+        loss = LossCrossEntropy()
+        last_activation = 'softmax'
+    else:
+        loss = LossMSE()
+        last_activation = activation
+        
+    model = Sequential()
+    
+    if num_of_hidden_layers > 0:
+        model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation=activation))
+        model.add(Dropout(prob=p_dropout))
+    
+        for i in range(num_of_hidden_layers-1):
+            model.add(Linear(out=num_of_neurons[i+2], activation=activation))
+            model.add(Dropout(prob=p_dropout))
 
-def default_net_1(input, target, num_of_neurons=(2, 25, 25, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    model = Sequential(
-        [
-            Linear(num_of_neurons[0], num_of_neurons[1]),
-            ReLU(),
-            Linear(num_of_neurons[1], num_of_neurons[2]),
-            ReLU(),
-            Linear(num_of_neurons[2], num_of_neurons[3]),
-            ReLU(),
-            Linear(num_of_neurons[3], num_of_neurons[4])
-        ]
-    )
+        model.add(Linear(out=num_of_neurons[-1], activation=last_activation))
+    
+    else:
+        model.add(Linear(out=num_of_neurons[-1], input_size=num_of_neurons[0], activation=last_activation))
+    
+    model.loss = loss
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
 
-    mse = LossMSE(target)
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
 
-    # integrate loss function to optimizer like in Keras
-    sgd = SGD(mse, lr, momentum_coef)
-    print(type(input), " -- ", type(model))
+    return model, report
 
-    sgd.train(model, input, num_of_epochs, verbose=0)
+def default_net_1(x_all, y_all, num_of_neurons=(2, 25, 2), activation='relu', lr=0.1, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    1 hidden layer, CE
+    """
+    ce = LossCrossEntropy()
 
-    return model, mse.loss_logging
+    model = Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation=activation))
+    model.add(Dropout(prob=p_dropout))
 
+    model.add(Linear(out=num_of_neurons[2], activation='softmax'))
 
-def default_net_2(input, target, num_of_neurons=(2, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    lin1 = Linear(num_of_neurons[0], num_of_neurons[1])
-    relu1 = ReLU()
-    lin2 = Linear(num_of_neurons[1], num_of_neurons[2])
-    mse = LossMSE(target)
+    model.loss = ce
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
 
-    seq = Sequential()
-    seq.add(lin1, name="Lin1")
-    seq.add(relu1, name="ReLU1")
-    seq.add(lin2, name="Lin2")
-    seq.add(mse, name="MSE")
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
 
-    sgd = SGD(lr, momentum_coef)
+    return model, report
 
-    sgd.train(input, seq, num_of_epochs)
+def default_net_1(x_all, y_all, num_of_neurons=(2, 25, 2), activation='relu', lr=0.1, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    1 hidden layer, CE
+    """
+    ce = LossCrossEntropy()
 
-    return seq, mse.loss_logging
+    model = Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation=activation))
+    model.add(Dropout(prob=p_dropout))
 
+    model.add(Linear(out=num_of_neurons[2], activation='softmax'))
 
-def default_net_3(input, target, num_of_neurons=(2, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    lin1 = Linear(num_of_neurons[0], num_of_neurons[1])
-    sig1 = Sigmoid()
-    lin2 = Linear(num_of_neurons[1], num_of_neurons[2])
-    sig2 = Sigmoid()
-    ce = LossCrossEntropy(target)
+    model.loss = ce
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
 
-    seq = Sequential()
-    seq.add(lin1, name="Lin1")
-    seq.add(sig1, name="Sigmoid1")
-    seq.add(lin2, name="Lin2")
-    seq.add(sig2, name="Sigmoid2")
-    seq.add(ce, name="CE")
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
 
-    sgd = SGD(lr, momentum_coef)
-
-    sgd.train(input, seq, num_of_epochs)
-
-    return seq, ce.loss_logging
-
-
-def default_net_4(input, target, num_of_neurons=(2, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    lin1 = Linear(num_of_neurons[0], num_of_neurons[1])
-    sig1 = Sigmoid()
-    ce = LossCrossEntropy(target)
-
-    seq = Sequential()
-    seq.add(lin1, name="Lin1")
-    seq.add(sig1, name="Sigmoid1")
-    seq.add(ce, name="CE")
-
-    sgd = SGD(lr, momentum_coef)
-
-    sgd.train(input, seq, num_of_epochs)
-
-    return seq, ce.loss_logging
+    return model, report
 
 
-def default_net_5(input, target, num_of_neurons=(2, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    lin1 = Linear(num_of_neurons[0], num_of_neurons[1])
-    mse = LossMSE(target)
+def default_net_2(x_all, y_all, num_of_neurons=(2, 25, 25, 2), activation='relu', lr=0.1, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    2 hidden layers, CE
+    """
+    ce = LossCrossEntropy()
 
-    seq = Sequential()
-    seq.add(lin1, name="Lin1")
-    seq.add(mse, name="MSE")
+    model = Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[2], activation=activation))
+    model.add(Dropout(prob=p_dropout))
 
-    sgd = SGD(lr, momentum_coef)
+    model.add(Linear(out=num_of_neurons[3], activation='softmax'))
 
-    sgd.train(input, seq, num_of_epochs)
+    model.loss = ce
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
 
-    return seq, mse.loss_logging
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
+
+    return model, report
 
 
-def default_net_6(input, target, num_of_neurons=(2, 25, 2), lr=0.1, momentum_coef=0.0, num_of_epochs=100):
-    lin1 = Linear(num_of_neurons[0], num_of_neurons[1])
-    tan1 = Tanh()
-    lin2 = Linear(num_of_neurons[1], num_of_neurons[2])
-    tan2 = Tanh()
-    mse = LossMSE(target)
+def default_net_3(x_all, y_all, num_of_neurons=(2, 25, 25, 25, 2), activation='relu', lr=0.1, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    3 hidden layers, CE
+    """
+    ce = LossCrossEntropy()
 
-    seq = Sequential()
-    seq.add(lin1, name="Lin1")
-    seq.add(tan1, name="Tan1")
-    seq.add(lin2, name="Lin2")
-    seq.add(tan2, name="Tan2")
-    seq.add(mse, name="MSE")
+    model = Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[2], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[3], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    
+    model.add(Linear(out=num_of_neurons[4], activation='softmax'))
 
-    sgd = SGD(lr, momentum_coef)
+    model.loss = ce
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
 
-    sgd.train(input, seq, num_of_epochs)
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
 
-    return seq, mse.loss_logging
+    return model, report
+
+
+def default_net_4(x_all, y_all, num_of_neurons=(2, 25, 2), activation='relu', lr=0.001, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    1 hidden layer, MSE
+    """
+    mse = LossMSE()
+    
+    model = modules.Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation='relu'))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[2], activation=activation))
+
+    model.loss = mse
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
+
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
+
+    return model, report
+
+def default_net_5(x_all, y_all, num_of_neurons=(2, 25, 25, 2), activation='relu', lr=0.001, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    2 hidden layers, MSE
+    """
+    mse = LossMSE()
+    
+    model = modules.Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation='relu'))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[2], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[3], activation=activation))
+
+    model.loss = mse
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
+
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
+
+    return model, report
+
+def default_net_6(x_all, y_all, num_of_neurons=(2, 25, 25, 25, 2), activation='relu', lr=0.001, momentum_coef=0.0, weight_decay=0.0, p_dropout=0.0, num_of_epochs=100, val_split=0.2, verbose=0):
+    """
+    3 hidden layers, MSE
+    """
+    mse = LossMSE()
+    
+    model = modules.Sequential()
+    model.add(Linear(out=num_of_neurons[1], input_size=num_of_neurons[0], activation='relu'))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[2], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[3], activation=activation))
+    model.add(Dropout(prob=p_dropout))
+    model.add(Linear(out=num_of_neurons[4], activation=activation))
+
+    model.loss = mse
+    sgd = SGD(lr, momentum_coef, weight_decay=weight_decay)
+
+    report = sgd.train(model, x_all, y_all, num_of_epochs, val_split=val_split, verbose=verbose)
+
+    return model, report
